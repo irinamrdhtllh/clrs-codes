66b08f8be0519049bd86c877ed1f6d318625a678
Irina M. S <irinamardhatillah@gmail.com>
2024-10-22 17:57:22 +0700

    Add unit test for insertion sort

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -6,8 +6,3 @@ def insertion_sort(A):
             A[j + 1] = A[j]
             j -= 1
         A[j + 1] = key
-
-
-A = [5, 2, 4, 6, 1, 3]
-insertion_sort(A)
-print(A)
---
+++ test/test_insertion_sort.py
@@ -0,0 +1,22 @@
+from algorithms.insertion_sort import insertion_sort
+
+
+def test_general_list():
+    actual = [5, 2, 4, 6, 1, 3]
+    insertion_sort(actual)
+    expected = [1, 2, 3, 4, 5, 6]
+    assert actual == expected
+
+
+def test_empty_list():
+    actual = []
+    insertion_sort(actual)
+    expected = []
+    assert actual == expected
+
+
+def test_single_element_list():
+    actual = [1]
+    insertion_sort(actual)
+    expected = [1]
+    assert actual == expected
